c5e31c51256f8cf72dac597c0852bfb780dd8b86
Canned Seagull <101097344+Canned-Seagull@users.noreply.github.com>
2026-01-26 22:40:01 +0800

    fix(serialiser): Handle spaces in tokens correctly

--- src/serialiser.ts
+++ src/serialiser.ts
@@ -3,7 +3,12 @@ import type { DataNode } from "./data_node.ts";
 // Serialise a data node into data file source
 export function serialiseDataNode(node: DataNode): string {
   function serialise(node: DataNode): string[] {
-    const line = node.tokens.map((token) => token.value).join(" ");
+    const line = node.tokens.map((token) => {
+      if (token.value.includes(" ")) {
+        if (token.value.includes('"')) return "`" + token.value + "`";
+        else return '"' + token.value + '"';
+      } else return token.value;
+    }).join(" ");
     const children = node.children
       .map(serialise)
       .flat()
